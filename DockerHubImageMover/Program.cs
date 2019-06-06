bf727a9f9bf8f9cf1343887248fc82edacd20b92
MichaelSimons <msimons@microsoft.com>
2019-06-06 13:02:43 -0500

    Misc changes

---
+++ DockerHubImageMover/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+
+namespace DockerHubImageMover
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string jsonContent = File.ReadAllText("Tags.json");
+            JObject json = JObject.Parse(jsonContent);
+
+            foreach (JToken image in json["data"])
+            {
+                string tag = (string)image["name"];
+                if (!tag.Contains("image-builder"))
+                {
+                    string dockerHubimage = $"docker.io/microsoft/dotnet-buildtools-prereqs:{tag}";
+                    string acrimage = $"public/dotnet-buildtools/prereqs:{tag}";
+
+                    // Execute($"pull {dockerHubimage}", autoRetry: true);
+                    // Execute($"push dotnetdocker.azurecr.io/{acrimage}", autoRetry: true);
+
+                    Execute($"az acr import -n dotnetdocker --source {dockerHubimage} -t {acrimage} --subscription fb511a11-c3d4-48f1-ba06-3ba13f270022", autoRetry: true);
+                }
+            }
+        }
+
+        private static string Execute(
+            string args, bool ignoreErrors = false, bool autoRetry = false)
+        {
+            (Process Process, string StdOut, string StdErr) result;
+            if (autoRetry)
+            {
+                result = ExecuteWithRetry(args, ExecuteProcess);
+            }
+            else
+            {
+                result = ExecuteProcess(args);
+            }
+
+            if (!ignoreErrors && result.Process.ExitCode != 0)
+            {
+                ProcessStartInfo startInfo = result.Process.StartInfo;
+                string msg = $"Failed to execute {startInfo.FileName} {startInfo.Arguments}{Environment.NewLine}{result.StdErr}";
+                throw new InvalidOperationException(msg);
+            }
+
+            return result.StdOut;
+        }
+
+        private static (Process Process, string StdOut, string StdErr) ExecuteProcess(
+            string args)
+        {
+            Process process = new Process
+            {
+                EnableRaisingEvents = true,
+                StartInfo =
+                {
+                    FileName = "powershell",
+                    Arguments = args,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                }
+            };
+
+            StringBuilder stdOutput = new StringBuilder();
+            process.OutputDataReceived += new DataReceivedEventHandler((sender, e) => stdOutput.AppendLine(e.Data));
+
+            StringBuilder stdError = new StringBuilder();
+            process.ErrorDataReceived += new DataReceivedEventHandler((sender, e) => stdError.AppendLine(e.Data));
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+
+            string output = stdOutput.ToString().Trim();
+            if (!string.IsNullOrWhiteSpace(output))
+            {
+                Console.WriteLine(output);
+            }
+
+            string error = stdError.ToString().Trim();
+            if (!string.IsNullOrWhiteSpace(error))
+            {
+                Console.WriteLine(error);
+            }
+
+            return (process, output, error);
+        }
+
+        private static (Process Process, string StdOut, string StdErr) ExecuteWithRetry(
+            string args,
+            Func<string, (Process Process, string StdOut, string StdErr)> executor)
+        {
+            const int maxRetries = 5;
+            const int waitFactor = 5;
+
+            int retryCount = 0;
+
+            (Process Process, string StdOut, string StdErr) result = executor(args);
+            while (result.Process.ExitCode != 0)
+            {
+                retryCount++;
+                if (retryCount >= maxRetries)
+                {
+                    break;
+                }
+
+                int waitTime = Convert.ToInt32(Math.Pow(waitFactor, retryCount - 1));
+                Console.WriteLine($"Retry {retryCount}/{maxRetries}, retrying in {waitTime} seconds...");
+
+                Thread.Sleep(waitTime * 1000);
+                result = executor(args);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Validate_WindowsTags.ps1
@@ -0,0 +1,68 @@
+function Log {
+    param ([string] $Message)
+
+    Write-Output $Message
+}
+
+function Exec {
+    param ([string] $Cmd)
+
+    Log "Executing: '$Cmd'"
+    Invoke-Expression $Cmd
+    if ($LASTEXITCODE -ne 0) {
+        throw "Failed: '$Cmd'"
+    }
+}
+
+function PullImages {
+    param ([string] $Repo1, [string] $Repo2, [string] $Tag)
+
+    Exec "docker pull ${Repo1}:$Tag"
+    Exec "docker pull ${Repo2}:$Tag"
+    Log "---------------------------------------------------------"
+}
+
+function CompareIds {
+    param ([string] $Repo1, [string] $Repo2, [string] $Tag)
+
+    $repo1Id = docker inspect --format="{{.Id}}" "${Repo1}:$Tag"
+    $repo2Id = docker inspect --format="{{.Id}}" "${Repo2}:$Tag"
+    if ($repo1Id -ne $repo2Id)
+    {
+        Write-Error "'${Repo1}:$Tag' and '${Repo2}:$Tag' are not equivalent"
+        Exec "docker history ${Repo1}:$Tag"
+        Exec "docker history ${Repo2}:$Tag"
+    }
+    else 
+    {
+        Write-Output "'${Repo1}:$Tag' and '${Repo2}:$Tag' are equivalent"
+        Exec "docker history ${Repo1}:$Tag"
+    }
+    Log "---------------------------------------------------------"
+}
+
+function CompareImages {
+    param ([string] $Repo1, [string] $Repo2, [string[]] $Tags)
+
+    $Tags |
+        ForEach-Object { PullImages -Repo1 $Repo1 -Repo2 $Repo2 -Tag $_ }
+
+    $Tags |
+        ForEach-Object { CompareIds -Repo1 $Repo1 -Repo2 $Repo2 -Tag $_}
+}
+
+$nanoTags = @("sac2016", "1709", "1803")
+$serverCoreTags = @("ltsc2016", "1709", "1803")
+
+CompareImages -Repo1 "microsoft/nanoserver" -Repo2 "mcr.microsoft.com/windows/nanoserver" -Tags $nanoTags
+
+CompareImages -Repo1 "mcr.microsoft.com/windows/servercore" -Repo2 "microsoft/windowsservercore" -Tags $serverCoreTags
+
+Exec "docker pull mcr.microsoft.com/windows/nanoserver:1809"
+Exec "docker history mcr.microsoft.com/windows/nanoserver:1809"
+Exec "docker pull mcr.microsoft.com/windows/servercore:1809"
+Exec "docker history mcr.microsoft.com/windows/servercore:1809"
+Exec "docker pull mcr.microsoft.com/windows/servercore:ltsc2019"
+Exec "docker history mcr.microsoft.com/windows/servercore:ltsc2019"
+Exec "docker pull mcr.microsoft.com/windows/nanoserver:1809-arm32v7"
+Exec "docker history mcr.microsoft.com/windows/nanoserver:1809-arm32v7"
